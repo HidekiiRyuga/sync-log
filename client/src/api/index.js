5749c32df4b0b2dc73b61752c8285cc32bb4a5ee
Stuti Jeisa Toppo <jnishaera@gmail.com>
2025-12-30 00:40:41 +0530

    Fix: Remove unused variable in api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
 
-const isProd = process.env.NODE_ENV === 'production';
 const url = '/logs'; 
 
-console.log("API is currently pointing to:", url);
-
 export const fetchLogs = () => axios.get(url);
-export const createLog = (newLog) => axios.post(url, newLog); 
+export const createLog = (newLog) => axios.post(url, newLog);
 export const likeLog = (id) => axios.patch(`${url}/${id}/likeLog`);
 export const updateLog = (id, updatedLog) => axios.patch(`${url}/${id}`, updatedLog);
 export const deleteLog = (id) => axios.delete(`${url}/${id}`);
