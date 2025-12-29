6c107e99bba83343f7232af8902ec8dfea551ccf
Stuti Jeisa Toppo <jnishaera@gmail.com>
2025-12-30 00:24:02 +0530

    FIX: Removed proxy from package.json

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Sync-log</Typography>
-        {/* If you have a sync-log icon, replace 'memories' here later */}
+        {}
       </AppBar>
       <Grow in>
         <Container>
